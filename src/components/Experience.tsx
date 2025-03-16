0517487e03018da8c66fedf9957d3c892f28a47f
Ash19-88 <ayelenllampa88@gmail.com>
2025-03-16 20:25:14 -0300

    feat: experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { logos } from "@/utils/logos";
+import { useTranslations } from "next-intl";
+import React from "react";
+import Image from "next/image";
 
-export default function Experience() {
+
+const Experience = () => {
+  const  t  = useTranslations("Experience");
+
   return (
-    <div>Experience</div>
-  )
-}
+    <div
+      id={t("experience")}
+      className="w-full h-full font-Raleway"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 lg:px-20 flex flex-col justify-center w-full text-white">
+        <div className="pt-20 lg:pt-16 2xl:pt-32">
+          <p className="text-2xl md:text-4xl font-bold border-b-4 border-slate-400 px-2 pb-2 inline ">
+            {t("experience")}
+          </p>
+          <p className="pt-4 pb-0 md:pt-8 text-xl md:text-2xl">
+            {t("experienceSubtitle")}
+          </p>
+        </div>
+
+        <div
+          className="w-full grid grid-cols-3 gap-5 sm:grid-cols-4 sm:gap-8 text-center py-6
+        px-0 md:gap-10 md:py-10"
+        >
+          {logos.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-110 pt-2 duration-500 rounded-lg ${style}`}
+            >
+              <Image src={src} alt={title} className="w-6 mx-auto sm:w-14" width={500}
+            height={300} />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/utils/logos.ts
@@ -0,0 +1,105 @@
+export type Logo = {
+    id: number;
+    src: string;
+    title: string;
+    style: string;
+}
+
+export const logos: Logo[] = [
+    {
+      id: 1,
+      src: "/html.png",
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: "/css.png",
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: "/javascript.png",
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: "/typescript.png",
+      title: "Typescript",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 5,
+      src: "/node.png",
+      title: "Node",
+      style: "shadow-green-500",
+    },
+    {
+      id: 6,
+      src: "/react.png",
+      title: "React",
+      style: "shadow-blue-400",
+    },
+    {
+      id: 7,
+      src: "/next-js.svg",
+      title: "Next JS",
+      style: "shadow-white",
+    },
+    {
+      id: 8,
+      src: "/express.png",
+      title: "Express",
+      style: "shadow-white",
+    },
+    {
+      id: 9,
+      src: "/bootstrap.png",
+      title: "Bootstrap",
+      style: "shadow-purple-500",
+    },
+    {
+      id: 10,
+      src: "/tailwind.png",
+      title: "TailwindCSS",
+      style: "shadow-blue-300",
+    },
+    {
+      id: 11,
+      src: "/github.png",
+      title: "GitHub",
+      style: "shadow-white",
+    },
+    {
+      id: 12,
+      src: "/postman.svg",
+      title: "Postman",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 13,
+      src: "/firebase.svg",
+      title: "Firebase",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 14,
+      src: "/figma.svg",
+      title: "Figma",
+      style: "shadow-pink-500",
+    },
+    {
+      id: 15,
+      src: "/visual-studio-code.svg",
+      title: "Visual Studio Code",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 16,
+      src: "/jira.svg",
+      title: "Jira",
+      style: "shadow-blue-500",
+    },
+  ];
