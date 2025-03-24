03ab3782f285fd541cbec2b66f84d3a642baefb4
Ash19-88 <ayelenllampa88@gmail.com>
2025-03-23 21:57:11 -0300

    fix: locale import

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,5 @@
 import {getRequestConfig} from 'next-intl/server';
-import {routing} from './routing';
+import {Locale, routing} from './routing';
  
 export default getRequestConfig(async ({requestLocale}) => {
   // This typically corresponds to the `[locale]` segment
