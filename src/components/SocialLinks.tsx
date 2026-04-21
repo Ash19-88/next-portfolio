e32253805889d2a45a167754b21c8ed64e02f963
Ash19-88 <ayelenllampa88@gmail.com>
2026-04-20 21:10:29 -0300

    feat: updating ui to matching new style

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -3,55 +3,38 @@ import React from "react";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
-
 const SocialLinks = () => {
-  const  t  = useTranslations("SocialLinks");
+  const t = useTranslations("SocialLinks");
   const links = [
     {
       id: 1,
-      child: (
-        <>
-          {t("linkedinLink")} <FaLinkedin size={30} />
-        </>
-      ),
+      child: (<>{t("linkedinLink")} <FaLinkedin size={25} /></>),
       href: "https://www.linkedin.com/in/ayelen-llampa1988",
     },
     {
       id: 2,
-      child: (
-        <>
-          {t("githubLink")} <FaGithub size={30} />
-        </>
-      ),
+      child: (<>{t("githubLink")} <FaGithub size={25} /></>),
       href: "https://github.com/Ash19-88",
     },
     {
       id: 3,
-      child: (
-        <>
-          {t("resumeLink")} <BsFillPersonLinesFill size={30} />
-        </>
-      ),
+      child: (<>{t("resumeLink")} <BsFillPersonLinesFill size={25} /></>),
       href: "/Ayelen-Llampa-resume.pdf",
-      style: "rounded-br-md",
       download: true,
     },
   ];
+
   return (
-    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed font-Raleway z-30">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed z-30">
       <ul>
-        {links.map(({ id, child, href, style, download }) => (
+        {links.map(({ id, child, href, download }) => (
           <li
             key={id}
-            className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-[#00000044]" +
-              " " +
-              style
-            }
+            className="flex justify-between items-center w-40 h-14 px-4 ml-[-105px] hover:ml-[-10px] duration-500 bg-zinc-950/40 backdrop-blur-md border border-white/5 hover:border-cyan-500/50 hover:shadow-[0_0_20px_rgba(34,211,238,0.2)] first:rounded-tr-xl last:rounded-br-xl"
           >
             <a
               href={href}
-              className="flex justify-between items-center w-full text-white"
+              className="flex justify-between items-center w-full text-slate-400 hover:text-white font-mono text-xs tracking-widest transition-colors"
               download={download}
               target="_blank"
               rel="noreferrer"
